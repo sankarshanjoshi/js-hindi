e15b65d9c5bccc0c09dc485acb739e44c8af84a6
Sankarshan Joshi <96820769+sankarshanjoshi@users.noreply.github.com>
2024-04-27 13:57:10 +0000

    done with conversion operations

---
+++ all the basics/conversionOperations.js
@@ -0,0 +1,13 @@
+let value = 3
+let negval = -value //-3
+
+let string = "hitesh"
+let string2 = "hit"
+let str3 = string+string2;
+//console.log(str3);
+
+console.log("1" + 2); //12
+console.log(1 + "2");//12
+console.log("1" + 2 + 2);//122
+console.log(1 + 2 + "2"); // buttttt 32
+//in documentation it is written that the first type of variable is preferred and others are converted to the same
