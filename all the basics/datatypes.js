71b1466bef04ec4c7fd9cde453a4b9f209100274
Sankarshan Joshi <96820769+sankarshanjoshi@users.noreply.github.com>
2024-04-27 13:29:02 +0000

    datatype and variables learnt

---
+++ all the basics/datatypes.js
@@ -0,0 +1,14 @@
+// number
+//bigInt
+//boolean
+let state = null // this will display null
+//null is a standalone values
+let sign //this will display undefined
+
+
+//undefined
+//symbol - gives uniqueness
+//object
+
+console.log(typeof state);//object
+console.log(typeof sign);//undefined
---
+++ all the basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "hitesh@gmail,com"
+var accountPass = "123.123"
+accountCity = "jaipur"
+
+console.log(accountEmail);
+//instead of doing console.log, console.table is the best option to get the data
+console.table([accountEmail,accountCity,accountPass])
+
