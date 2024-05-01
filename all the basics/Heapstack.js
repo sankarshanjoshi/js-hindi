53c47d7553e55f1fb4bb9cbc225c297fae636d4d
Sankarshan Joshi <96820769+sankarshanjoshi@users.noreply.github.com>
2024-05-01 19:07:33 +0000

    learnt strings

---
+++ all the basics/Heapstack.js
@@ -0,0 +1,9 @@
+//as we know variables dont posses the values of the data but just are a reference and point to the data present in the heap
+let anothername = "myYoutubeName";
+let namey = anothername;
+
+console.log(anothername);
+console.log(namey);
+
+//the reference is same for both variable so making anychanges to them will get changed in both of them
+
---
+++ all the basics/String.js
@@ -0,0 +1,23 @@
+let name = "sanku"
+let surname = "joshi"
+console.log(name+" "+surname);//this is primitive
+
+//instead use this
+//make use of backtick ``
+console.log(`my name is ${name} and my surname is ${surname}`);
+
+//can also use toUpperCase
+console.log(`my name is ${name.toUpperCase()} and my surname is ${surname}`);
+
+
+
+
+//one more effective way of declaring array is using new keyword
+let nameone = new String('haha this is also sankarshan');
+console.log(nameone);
+//it returns something different as telling it is string 
+
+//some functions related to string
+console.log(nameone.length); 
+console.log(nameone[0]);
+console.log(nameone.charAt(3));
